9448be3847e1e231dbb5cce14aff9608f5ab4608
ghm7 <pozzisgnosis@outlook.com>
2023-08-05 22:47:40 -0300

    Solucion al problema del boton

--- eventos.js
+++ eventos.js
@@ -1,4 +1,12 @@
 const div = document.querySelector("div");
+const button = document.querySelector("button");
+
 div.addEventListener("click", (e) => {
+    e.stopPropagation();
     alert("Hola! Soy el div");
+});
+
+button.addEventListener("click", (e) => {
+    e.stopPropagation();
+    alert("Hola! Soy el botón");
 });
